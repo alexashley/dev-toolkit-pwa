8f494986a050677f48f26cde4733b6a3c2bbd010
Alex Ashley <alexashley15@gmail.com>
2019-02-03 20:09:36 -0600

    Footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,49 @@
 import React from 'react';
 
+const Link = ({ href, children }) => (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+        {children}
+    </a>
+);
+
 const Footer = () => {
     return (
         <footer className="footer">
-            <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects">
-                MDN JavaScript
-            </a>
+            <nav>
+                <div>
+                    <h4>References</h4>
+                    <ul>
+                        <li>
+                            <Link href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects">
+                                MDN
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet">
+                                GFM Cheatsheet
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Misc</h4>
+                    <ul>
+                        <li>
+                            <Link href="http://news.ycombinator.com">HN</Link>
+                        </li>
+                        <li>
+                            <Link href="https://github.com/alexashley">
+                                GitHub
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="https://gist.github.com/alexashley">
+                                Gists
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
             <span>{`version: ${process.env.REACT_APP_VERSION}`}</span>
         </footer>
     );
